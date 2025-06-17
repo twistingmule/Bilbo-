cc04b8ce49755b790cc496394afba8931877da42
twistingmule <twistingmule55@gmail.com>
2025-06-17 15:35:56 -0500

    main.py

--- main.py
+++ main.py
@@ -51,7 +51,7 @@ def home():
 def run_bot():
     bot.run(os.getenv("DISCORD_TOKEN"))
 
-if __name__ == "__main__":
+if __name__ == "__main__": bilbo
     threading.Thread(target=run_bot).start()
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
